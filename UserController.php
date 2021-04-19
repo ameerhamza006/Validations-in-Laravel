67f8c516799a583330c86ab5f4ef6e89d7d262b6
Ameer Hamza <76493021+ameerhamza006@users.noreply.github.com>
2021-04-19 12:05:17 +0500

    Create UserController.php

---
+++ UserController.php
@@ -0,0 +1,11 @@
+public function insert(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:10',
+            'email' => 'required|unique:email',
+            'phone' => 'required|numaric',
+            'password' => 'required|min:6'
+        ]);
+
+        return view('abc');
+    }
